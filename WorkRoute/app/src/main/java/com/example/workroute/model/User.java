9f7b382a6c3cf7f46286603c3fa112548dd355e0
AndreiMihali <mihaliandrei50@gmail.com>
2022-03-15 14:12:47 +0100

    Añadiendo colores campos de login y añadiendo pantalla de regsitro. Flata añadir funcionalidad a todos los botones

--- WorkRoute/app/src/main/java/com/example/workroute/model/User.java
+++ WorkRoute/app/src/main/java/com/example/workroute/model/User.java
@@ -15,6 +15,131 @@ public class User {
     private boolean conductor;
     private int vecesConectadas;
 
+    public User(String uid, String nombre, int edad, String localidad, String direccion, int nivel, boolean activo, boolean suscrito, int nivelSuscripcion, String fotoPerfil, String alias, boolean conductor, int vecesConectadas) {
+        this.uid = uid;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.localidad = localidad;
+        this.direccion = direccion;
+        this.nivel = nivel;
+        this.activo = activo;
+        this.suscrito = suscrito;
+        this.nivelSuscripcion = nivelSuscripcion;
+        this.fotoPerfil = fotoPerfil;
+        this.alias = alias;
+        this.conductor = conductor;
+        this.vecesConectadas = vecesConectadas;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getLocalidad() {
+        return localidad;
+    }
+
+    public void setLocalidad(String localidad) {
+        this.localidad = localidad;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    public boolean isSuscrito() {
+        return suscrito;
+    }
+
+    public void setSuscrito(boolean suscrito) {
+        this.suscrito = suscrito;
+    }
+
+    public int getNivelSuscripcion() {
+        return nivelSuscripcion;
+    }
+
+    public void setNivelSuscripcion(int nivelSuscripcion) {
+        this.nivelSuscripcion = nivelSuscripcion;
+    }
+
+    public String getFotoPerfil() {
+        return fotoPerfil;
+    }
+
+    public void setFotoPerfil(String fotoPerfil) {
+        this.fotoPerfil = fotoPerfil;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public boolean isConductor() {
+        return conductor;
+    }
+
+    public void setConductor(boolean conductor) {
+        this.conductor = conductor;
+    }
+
+    public int getVecesConectadas() {
+        return vecesConectadas;
+    }
+
+    public void setVecesConectadas(int vecesConectadas) {
+        this.vecesConectadas = vecesConectadas;
+    }
+
+    public User(){}
+
+
+
+
 
 
 }
