b17638823c3d6bce0a36b5246888f6059532551d
RubenDev7 <97059915+RubenDev7@users.noreply.github.com>
2022-03-23 12:35:35 +0100

    coger correo de usuario para mostrarlo en perfil

--- WorkRoute/app/src/main/java/com/example/workroute/activitys/Profile.java
+++ WorkRoute/app/src/main/java/com/example/workroute/activitys/Profile.java
@@ -5,12 +5,16 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.workroute.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Profile extends AppCompatActivity {
-
+    TextView userName;
     Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.Register);
@@ -22,11 +26,22 @@ public class Profile extends AppCompatActivity {
     private void main() {
         controls();
         back();
+        getUser();
     }
 
     private void controls() {
         toolbar=findViewById(R.id.toolbar);
+        userName = findViewById(R.id.name);
     }
+
+    private void getUser() {
+        //Se coge el email de usuario de la cuenta, ya sea google o email
+        userName.setText(FirebaseAuth.getInstance().getCurrentUser().getEmail());
+        //Con getDisplayName no coge el nombre de usuario si no es de cuenta de google
+        //habría que mandarlo con preferencias.
+    }
+
+
 
     private void back() {
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
@@ -34,6 +49,7 @@ public class Profile extends AppCompatActivity {
             public void onClick(View v) {
                 //regresar al main
                 finish();
+
             }
         });
     }
