1fb95cd6d31d09b37a134a74cd376c474d035e6d
AndreiMihali <mihaliandrei50@gmail.com>
2022-03-25 21:03:56 +0100

    AÑADIENDO VENTANAS RESTANTES

---
+++ WorkRoute/app/src/main/java/com/example/workroute/activitys/Notifications.java
@@ -0,0 +1,15 @@
+package com.example.workroute.activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.workroute.R;
+
+public class Notifications extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notifications);
+    }
+}
