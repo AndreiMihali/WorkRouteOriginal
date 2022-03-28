b5fc9bb8088aec0561e577430fab53322e92ae51
AndreiMihali <mihaliandrei50@gmail.com>
2022-03-28 19:26:28 +0200

    ARREGLAO

--- WorkRoute/app/src/main/java/com/example/workroute/activitys/MainActivity.java
+++ WorkRoute/app/src/main/java/com/example/workroute/activitys/MainActivity.java
@@ -23,6 +23,7 @@ import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -57,7 +58,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import java.security.Provider;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
 
     private static final int CODE_UBI = 100;
     private FloatingActionButton button_menu, button_chats, button_profile, button_notifications, button_settings, button_close, button_ubi;
@@ -80,16 +81,308 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
 
-        /*****************************************************************************************************************************
-        * UTILIZAMOS LA PRIMERA LINEA DE CÓDIGO PARA HACER QUE EL MAPA OCUPE LA PANTALLA ENTERA
-        * UTILIZAMOS EL MAP FRAGMENT PARA INICIAR EL MAPA DE GGOGLE. PARA ESO PREVIAMENTE HABREMOS INCLUIDO LA API KEY DEL GGOGLE MAPS
-        * QUE SE ENCUENTRA EN LA CARPETA STRINGS
-        * UNA VEZ INICIADO EL MAPA INICIAMOS LAS VARIABLES Y COMPROBAMOS QUE SE TENGAN LOS PERMISOS DE UBICACIÓN
-        *******************************************************************************************************************************/
+        init();
+    }
+    private void init(){
+        addActivityResultLauncher();
+        button_menu = findViewById(R.id.buttonMenu);
+        button_chats = findViewById(R.id.buttonMessages);
+        button_profile = findViewById(R.id.buttonProfile);
+        button_notifications = findViewById(R.id.button_notifications);
+        button_settings = findViewById(R.id.buttonSettings);
+        button_close = findViewById(R.id.buttonSignOut);
+        button_ubi=findViewById(R.id.buttonUbi);
+        open = AnimationUtils.loadAnimation(this,R.anim.open_menu);
+        close = AnimationUtils.loadAnimation(this,R.anim.close_menu);
+        firebaseAuth=FirebaseAuth.getInstance();
+        firestore=FirebaseFirestore.getInstance();
+        progressDialog=new ProgressDialog(this,R.style.ProgressDialog);
+        rotateForward = AnimationUtils.loadAnimation(this,R.anim.rotate_forward);
+        rotateBackWard = AnimationUtils.loadAnimation(this,R.anim.rotate_backward);
+        progressDialog.setMessage("Checking your location. This can take a while...");
+        progressDialog.setCanceledOnTouchOutside(false);
+        iniciarMapa();
+        getUserData();
+        initListeners();
+    }
+
+    private void initListeners() {
+        /** MÉTODO PARA AÑADIR ICONOS DE NOTIFICACIÓN A LOS BOTONES*/
+        button_notifications.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @SuppressLint("UnsafeOptInUsageError")
+            @Override
+            public void onGlobalLayout() {
+                BadgeDrawable badgeDrawable = BadgeDrawable.create(MainActivity.this);
+                badgeDrawable.setNumber(2);
+                //Important to change the position of the Badge
+                badgeDrawable.setHorizontalOffset(30);
+                badgeDrawable.setVerticalOffset(20);
+
+                BadgeUtils.attachBadgeDrawable(badgeDrawable, button_notifications, null);
+
+                button_notifications.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+            }
+        });
+
+        button_menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                animateMenu();
+            }
+        });
+
+        button_chats.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Chats.class);
+                startActivity(i);
+                animateMenu();
+            }
+        });
+
+        button_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Profile.class);
+                startActivity(i);
+                animateMenu();
+            }
+        });
+
+        button_ubi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveToLocation();
+            }
+        });
+
+        //TODO : FALTAN LOS LISTENERS DEL RESTO DE BOTONES
 
 
+        button_close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(MainActivity.this,LoginActivity.class));
+                finish();
+                Toast.makeText(getApplicationContext(),"Saludos del menu 333", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void animateMenu() {
+        if (isOpen) {
+            button_menu.startAnimation(rotateForward);
+            button_menu.setImageResource(R.drawable.ic_baseline_menu_24);
+
+
+            button_chats.startAnimation(close);
+            button_profile.startAnimation(close);
+            button_notifications.startAnimation(close);
+            button_settings.startAnimation(close);
+            button_close.startAnimation(close);
+
+            button_chats.setVisibility(View.INVISIBLE);
+            button_profile.setVisibility(View.INVISIBLE);
+            button_notifications.setVisibility(View.INVISIBLE);
+            button_settings.setVisibility(View.INVISIBLE);
+            button_close.setVisibility(View.INVISIBLE);
+
+            button_chats.setClickable(false);
+            button_profile.setClickable(false);
+            button_notifications.setClickable(false);
+            button_settings.setClickable(false);
+            button_close.setClickable(false);
+
+            isOpen=false;
+        } else {
+            button_menu.startAnimation(rotateBackWard);
+            button_menu.setImageResource(R.drawable.ic_baseline_menu_open_24);
+            button_chats.startAnimation(open);
+            button_profile.startAnimation(open);
+            button_notifications.startAnimation(open);
+            button_settings.startAnimation(open);
+            button_close.startAnimation(open);
+
+
+            button_chats.setVisibility(View.VISIBLE);
+            button_profile.setVisibility(View.VISIBLE);
+            button_notifications.setVisibility(View.VISIBLE);
+            button_settings.setVisibility(View.VISIBLE);
+            button_close.setVisibility(View.VISIBLE);
+
+            button_chats.setClickable(true);
+            button_profile.setClickable(true);
+            button_notifications.setClickable(true);
+            button_settings.setClickable(true);
+            button_close.setClickable(true);
+            isOpen=true;
+        }
+    }
+
+    /**
+     * *********************************************************************************************************************************
+     * *********************************************************************************************************************************
+     * *********************************************************************************************************************************
+     *
+     *                             AQUI EMPIEZAN LOS METODOS QUE TIENEN QUE VER CON EL MAPA DE LOS COJONES
+     *
+     * *********************************************************************************************************************************
+     * *********************************************************************************************************************************
+     ***********************************************************************************************************************************/
+
+    private void addActivityResultLauncher() {
+        activityResultLauncher=registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if(result.getResultCode()==RESULT_CANCELED){
+                            getCurrentLocation();
+                        }
+                    }
+                }
+        );
+    }
+
+    private void iniciarMapa(){
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentByTag("mapFragment");
+        mapFragment.getMapAsync(this::onMapReady);
+    }
+
+    @SuppressLint("MissingPermission")
+    private void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        map.setMyLocationEnabled(true);
+        map.getUiSettings().setMyLocationButtonEnabled(false);
+        map.getUiSettings().setCompassEnabled(false);
+        map.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+        getCurrentLocation();
     }
 
 
+    private boolean isLocationEnabled(){
+        locationManager=(LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)||locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getCurrentLocation() {
+        progressDialog.show();
+        if(isLocationEnabled()){
+            criteria = new Criteria();
+            bestProvider = String.valueOf(locationManager.getBestProvider(criteria, true));
+            locationManager.requestLocationUpdates(bestProvider,1000,0, (LocationListener) this);
+        }else{
+            showDialogMessageGpsEnable();
+        }
+    }
+
+    private void showDialogMessageGpsEnable(){
+        progressDialog.dismiss();
+        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(this,R.style.DialogAlert);
+        builder.setMessage("To get your current location, you need to enable the GPS. Do you want enable it?");
+        builder.setTitle("GPS DISABLED");
+        builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                enableGps();
+            }
+        });
+        builder.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                progressDialog.dismiss();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+
+    private void enableGps() {
+        Intent settingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        activityResultLauncher.launch(settingsIntent);
+    }
+
+    private void moveToLocation() {
+        if(isLocationEnabled()){
+            map.animateCamera(CameraUpdateFactory.newLatLngZoom(ubiActual,15f),2000,null);
+        }else{
+            showDialogMessageGpsEnable();
+        }
+    }
+
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        locationManager.removeUpdates(this);
+        ubiActual=new LatLng(location.getLatitude(),location.getLongitude());
+        progressDialog.dismiss();
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(ubiActual,15f),2000,null);
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull List<Location> locations) {
+        LocationListener.super.onLocationChanged(locations);
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+        LocationListener.super.onProviderEnabled(provider);
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+        LocationListener.super.onProviderDisabled(provider);
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(locationManager!=null){
+            locationManager.removeUpdates(this);
+        }
+    }
+
+    /**
+     * *********************************************************************************************************************************
+     * *********************************************************************************************************************************
+     * *********************************************************************************************************************************
+     *
+     *                             AQUI TERMINAN LOS METODOS QUE TIENEN QUE VER CON EL MAPA DE LOS COJONES
+     *
+     * *********************************************************************************************************************************
+     * *********************************************************************************************************************************
+     ***********************************************************************************************************************************/
+
+    private void getUserData(){
+        new Handler().post(new Runnable() {
+            @Override
+            public void run() {
+                firestore.collection("Usuarios").document(firebaseAuth.getCurrentUser().getUid()).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        Companion.user = documentSnapshot.toObject(User.class);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("ERROR AL OBTENER LOS DATOS DEL USUARIO","ERROR AL OBTENER LOS DATOS DEL USUARIO "+e);
+                    }
+                });
+            }
+        });
+    }
 }
