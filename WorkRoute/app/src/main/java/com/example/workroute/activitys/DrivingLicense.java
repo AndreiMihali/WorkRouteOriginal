ad365a18207aee2a864c151287c7f7b57c55886d
RubenDev7 <97059915+RubenDev7@users.noreply.github.com>
2022-04-09 17:42:06 +0200

    pay method "hecho", drive license a falta de cambios para que coja cada foto en su sitio bien

--- WorkRoute/app/src/main/java/com/example/workroute/activitys/DrivingLicense.java
+++ WorkRoute/app/src/main/java/com/example/workroute/activitys/DrivingLicense.java
@@ -1,16 +1,43 @@
 package com.example.workroute.activitys;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.app.Instrumentation;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
 import com.example.workroute.R;
 import com.google.android.material.appbar.MaterialToolbar;
 
 public class DrivingLicense extends AppCompatActivity {
+
     MaterialToolbar toolbarDrivingLicense;
+    private ImageView imgFront;
+    private ImageView imgReverse;
+    private Button selectFront;
+    private Button selectReverse;
+    private Button saveLicense;
+
+    private static final int PICK_IMAGE = 100;
+    Uri imageUri;
+    private int numFotos = 0;
+    ActivityResultLauncher <Intent> activityResultLauncher;
+    private static final int PERMISSION_CODE_GALLERY = 100;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +50,39 @@ public class DrivingLicense extends AppCompatActivity {
     private void main() {
         controls();
         listeners();
+        initActivityResultLauncher();
     }
 
+    private void initActivityResultLauncher() {
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+
+                if (result.getResultCode()==RESULT_OK) {
+                    Intent foto=result.getData();
+                    int resultadoFoto = foto.getIntExtra("foto",0);
+                    imageUri=(Uri)foto.getData();
+                    if (resultadoFoto==1) {
+                        imgFront.setImageURI(imageUri);
+                    } else if (resultadoFoto==2){
+                        imgReverse.setImageURI(imageUri);
+                    }
+                } else if (result.getResultCode()==RESULT_CANCELED) {
+                    Toast.makeText(getApplicationContext(),"Cancelled", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
 
 
     private void controls() {
         toolbarDrivingLicense = findViewById(R.id.toolbar);
+        imgFront = findViewById(R.id.imageCarnetFrente);
+        imgReverse = findViewById(R.id.imageCarnetReverso);
+        selectFront = findViewById(R.id.buttonFront);
+        selectReverse = findViewById(R.id.buttonReverse);
+        saveLicense = findViewById(R.id.buttonSaveLicense);
     }
 
 
@@ -40,13 +94,57 @@ public class DrivingLicense extends AppCompatActivity {
                 finish();
             }
         });
+
+        selectFront.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirGaleria(1);
+            }
+        });
+
+        selectReverse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirGaleria(2);
+            }
+        });
+
     }
+
+    private void abrirGaleria(int num) {
+        int  permissionCheck= ContextCompat.checkSelfPermission(this,android.Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        if(permissionCheck!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE},PERMISSION_CODE_GALLERY);
+        }else{
+            llamarGaleria(num);
+        }
+    }
+
+    private void llamarGaleria(int num) {
+        Intent gallery=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        gallery.putExtra("foto",num);
+        activityResultLauncher.launch(gallery);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSION_CODE_GALLERY: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    llamarGaleria(0);
+                }
+            }
+            break;
+        }
+        return;
+    }
+
 
     @Override
     protected void onDestroy() {
         new MainActivity.Destroy(DrivingLicense.this).start();
         super.onDestroy();
     }
-
-
 }
--- WorkRoute/app/src/main/java/com/example/workroute/activitys/PayMethod.java
+++ WorkRoute/app/src/main/java/com/example/workroute/activitys/PayMethod.java
@@ -3,15 +3,27 @@ package com.example.workroute.activitys;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.workroute.R;
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textview.MaterialTextView;
+
+import java.util.Locale;
 
 public class PayMethod extends AppCompatActivity {
 
     MaterialToolbar toolbarPayMethods;
-
+    TextInputEditText inputName;
+    TextInputEditText inputCardNumber;
+    TextInputEditText inputCardMonth;
+    TextInputEditText inputCardYear;
+    TextInputEditText inputCardCVV;
+    Button savePayMethod;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +46,17 @@ public class PayMethod extends AppCompatActivity {
 
     private void controls() {
         toolbarPayMethods = findViewById(R.id.toolbarPay);
+        inputName = findViewById(R.id.inputFullName);
+        inputCardNumber = findViewById(R.id.inputCardNumber);
+        inputCardMonth = findViewById(R.id.inputMonth);
+        inputCardYear = findViewById(R.id.inputYear);
+        inputCardCVV = findViewById(R.id.inputCVV);
+        savePayMethod = findViewById(R.id.buttonSave);
     }
 
 
     private void listeners() {
+
         toolbarPayMethods.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -45,6 +64,42 @@ public class PayMethod extends AppCompatActivity {
                 finish();
             }
         });
+
+        savePayMethod.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (inputName.getText().toString().trim().equals("")
+                    || inputCardNumber.getText().toString().equals("")
+                    || inputCardNumber.getText().toString().length()!=16
+                    || inputCardMonth.getText().toString().length()!=2
+                    || inputCardYear.getText().toString().length()!=2
+                    || inputCardCVV.getText().toString().length()!=3
+                ) {
+                    Toast.makeText(getApplicationContext(),"Error while validating, check all fields", Toast.LENGTH_SHORT).show();
+                } else {
+                    //AQUI HAY QUE RECOGER LOS CAMPOS PARA MÁS ADELANTE
+                    Toast.makeText(getApplicationContext(),"Payment method saved successfully", Toast.LENGTH_SHORT).show();
+                    Handler h = new Handler();
+                    h.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            clearFields();
+                        }
+                    },2000);
+                }
+            }
+        });
+
     }
+
+    private void clearFields() {
+        inputName.setText("");
+        inputCardNumber.setText("");
+        inputCardMonth.setText("");
+        inputCardYear.setText("");
+        inputCardCVV.setText("");
+    }
+
 
 }
