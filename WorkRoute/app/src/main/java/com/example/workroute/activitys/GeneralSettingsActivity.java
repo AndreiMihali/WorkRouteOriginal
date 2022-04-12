7c4bd95bbcbbd00bceb6249426485b12a99ac45a
RubenDev7 <97059915+RubenDev7@users.noreply.github.com>
2022-04-12 14:48:54 +0200

    diseño y algo de codigo del settings, va tomando forma también

--- WorkRoute/app/src/main/java/com/example/workroute/activitys/GeneralSettingsActivity.java
+++ WorkRoute/app/src/main/java/com/example/workroute/activitys/GeneralSettingsActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.example.workroute.R;
 import com.example.workroute.network.callback.NetworkCallback;
@@ -14,8 +17,16 @@ import com.google.android.material.appbar.MaterialToolbar;
 public class GeneralSettingsActivity extends AppCompatActivity {
 
     private MaterialToolbar toolbar;
+
+    private CheckBox emailBox;
+    private CheckBox appBox;
+
     private RadioButton mapView1;
     private RadioButton mapView2;
+    private RadioGroup myGroup;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.Register);
@@ -32,11 +43,11 @@ public class GeneralSettingsActivity extends AppCompatActivity {
 
     private void controls() {
         toolbar = findViewById(R.id.toolbarSettings);
+        emailBox = findViewById(R.id.checkBoxEmailNotifications);
+        appBox = findViewById(R.id.checkBoxAppNotifications);
         mapView1 = findViewById(R.id.radioMapView1);
         mapView2 = findViewById(R.id.radioMapView2);
-
-
-
+        myGroup = findViewById(R.id.radioGroupMapViews);
 
 
     }
@@ -49,6 +60,60 @@ public class GeneralSettingsActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        emailBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+
+                if (emailBox.isChecked()) {
+                    //TODO: CODIGO QUE SE QUIERA METER
+                    Toast.makeText(getApplicationContext(),"email siii",Toast.LENGTH_SHORT).show();
+
+                } else {
+                    //TODO: CODIGO QUE SE QUIERA METER
+                    Toast.makeText(getApplicationContext(),"email nooo",Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
+
+        appBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+
+                if (appBox.isChecked()) {
+                    //TODO: CODIGO QUE SE QUIERA METER
+                    Toast.makeText(getApplicationContext(),"app siii",Toast.LENGTH_SHORT).show();
+
+                } else {
+                    //TODO: CODIGO QUE SE QUIERA METER
+                    Toast.makeText(getApplicationContext(),"app nooo",Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
+
+        myGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+
+            if (mapView1.isChecked()) {
+                //TODO: CODIGO QUE SE QUIERA METER PARA EL CAMBIO DE MAPA O LO QUE SEA
+                Toast.makeText(getApplicationContext(),"view1",Toast.LENGTH_SHORT).show();
+            }
+
+            if (mapView2.isChecked()) {
+                //TODO: CODIGO QUE SE QUIERA METER PARA EL CAMBIO DE MAPA O LO QUE SEA
+                Toast.makeText(getApplicationContext(),"view2",Toast.LENGTH_SHORT).show();
+            }
+
+            }
+        });
+
+
+
     }
 
     @Override
