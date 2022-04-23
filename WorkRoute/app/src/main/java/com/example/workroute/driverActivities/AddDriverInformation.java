e189e816f0a86cdaee4c2c43d64b1aaf0dd2e20f
AndreiMihali <mihaliandrei50@gmail.com>
2022-04-24 01:53:34 +0200

    canadido cosas. falta acabar

--- WorkRoute/app/src/main/java/com/example/workroute/driverActivities/AddDriverInformation.java
+++ WorkRoute/app/src/main/java/com/example/workroute/driverActivities/AddDriverInformation.java
@@ -15,6 +15,9 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -26,11 +29,34 @@ import com.google.android.material.snackbar.Snackbar;
 
 public class AddDriverInformation extends AppCompatActivity {
 
+    private AutoCompleteTextView autoCompleteColors,autoCompleteCars;
+    private ArrayAdapter<String> adapterColors;
+    private ArrayAdapter<String> adapterTypes;
+    private String colors[]={"Red","Blue","Black","White","Gray","Green","Yellow","Brown","Pink"};
+    private String types[]={"HatchBack","Sedan","Limousine","Cabriolet","Minibus","Minivan","Crossover","Jeep","Pickup"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.backDriverInformation);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_driver_information);
+        init();
+    }
+
+    private void init(){
+        autoCompleteCars=findViewById(R.id.autocompleteTypes);
+        autoCompleteColors=findViewById(R.id.autocompleteColors);
+        adapterColors=new ArrayAdapter<String>(this,R.layout.list_item,colors);
+        adapterTypes=new ArrayAdapter<String>(this,R.layout.list_item,types);
+
+        autoCompleteCars.setAdapter(adapterTypes);
+        autoCompleteColors.setAdapter(adapterColors);
+
+        initListeners();
+    }
+
+    private void initListeners() {
+
     }
 
 }
