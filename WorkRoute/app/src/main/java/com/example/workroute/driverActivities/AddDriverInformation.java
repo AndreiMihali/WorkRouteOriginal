951d6dc6bf9435cbe02caca67833bbb2c791b8a8
AndreiMihali <mihaliandrei50@gmail.com>
2022-04-24 20:01:34 +0200

    anadido conductor a base de datos

--- WorkRoute/app/src/main/java/com/example/workroute/driverActivities/AddDriverInformation.java
+++ WorkRoute/app/src/main/java/com/example/workroute/driverActivities/AddDriverInformation.java
@@ -9,11 +9,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -24,8 +27,18 @@ import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.example.workroute.R;
+import com.example.workroute.activitys.DriverMap;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.button.MaterialButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class AddDriverInformation extends AppCompatActivity {
 
@@ -34,6 +47,9 @@ public class AddDriverInformation extends AppCompatActivity {
     private ArrayAdapter<String> adapterTypes;
     private String colors[]={"Red","Blue","Black","White","Gray","Green","Yellow","Brown","Pink"};
     private String types[]={"HatchBack","Sedan","Limousine","Cabriolet","Minibus","Minivan","Crossover","Jeep","Pickup"};
+    private TextInputEditText documentId,plateNumber;
+    private MaterialButton btnContinue;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,15 +64,74 @@ public class AddDriverInformation extends AppCompatActivity {
         autoCompleteColors=findViewById(R.id.autocompleteColors);
         adapterColors=new ArrayAdapter<String>(this,R.layout.list_item,colors);
         adapterTypes=new ArrayAdapter<String>(this,R.layout.list_item,types);
-
+        documentId=findViewById(R.id.ed_idNumber);
+        plateNumber=findViewById(R.id.ed_plate);
         autoCompleteCars.setAdapter(adapterTypes);
         autoCompleteColors.setAdapter(adapterColors);
-
+        btnContinue=findViewById(R.id.btn_continue);
+        progressDialog=new ProgressDialog(this,R.style.ProgressDialog);
+        progressDialog.setMessage("Please wait...");
         initListeners();
     }
 
     private void initListeners() {
+        btnContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isAllFieldsOk()){
+                    progressDialog.show();
+                    setDataInDatabase();
+                }else{
+                    Snackbar.make(v,"You must need fill all fields",Snackbar.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
 
+    private boolean isAllFieldsOk(){
+        if(autoCompleteColors.getText().toString().isEmpty()
+            ||autoCompleteCars.getText().toString().isEmpty()
+            ||plateNumber.getText().toString().isEmpty()
+            ||documentId.getText().toString().isEmpty()){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    private void setDataInDatabase(){
+        new Handler().post(new Runnable() {
+            @Override
+            public void run() {
+                HashMap<String,Object> hashMap2=new HashMap<>();
+                hashMap2.put("DocumentId",documentId.getText().toString().trim());
+                hashMap2.put("PlateNumber",plateNumber.getText().toString().trim());
+                hashMap2.put("CarType",autoCompleteCars.getText().toString().trim());
+                hashMap2.put("CarColor",autoCompleteColors.getText().toString().trim());
+                FirebaseDatabase.getInstance().getReference().child("Usuarios").child(FirebaseAuth.getInstance().getUid()).child("DriverInformation").setValue(hashMap2).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        HashMap<String,Object> hashMap=new HashMap<>();
+                        hashMap.put("conductor","true");
+                        FirebaseDatabase.getInstance().getReference().child("Usuarios").child(FirebaseAuth.getInstance().getUid()).updateChildren(hashMap).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                HashMap<String,Object> hashMap=new HashMap<>();
+                                hashMap.put("conductor",true);
+                                FirebaseFirestore.getInstance().collection("Usuarios").document(FirebaseAuth.getInstance().getCurrentUser().getUid()).update(hashMap).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        progressDialog.dismiss();
+                                        startActivity(new Intent(AddDriverInformation.this, DriverMap.class));
+                                        finish();
+                                    }
+                                });
+                            }
+                        });
+                    }
+                });
+            }
+        });
     }
 
 }
