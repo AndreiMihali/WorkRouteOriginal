a8c8698e3feb4009db9afa56b84006e33ea491e1
RubenDev7 <97059915+RubenDev7@users.noreply.github.com>
2022-04-26 19:46:53 +0200

    personal_information -> diseño puede valer; Falta coger los datos que se muestran (foto,email,name,pass)

--- WorkRoute/app/build/generated/data_binding_base_class_source_out/debug/out/com/example/workroute/databinding/ActivityPersonalInformationBinding.java
+++ WorkRoute/app/build/generated/data_binding_base_class_source_out/debug/out/com/example/workroute/databinding/ActivityPersonalInformationBinding.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
@@ -15,6 +16,8 @@ import androidx.viewbinding.ViewBindings;
 import com.example.workroute.R;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.card.MaterialCardView;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import java.lang.NullPointerException;
 import java.lang.Override;
 import java.lang.String;
@@ -24,10 +27,34 @@ public final class ActivityPersonalInformationBinding implements ViewBinding {
   private final RelativeLayout rootView;
 
   @NonNull
+  public final MaterialCardView cardName;
+
+  @NonNull
+  public final MaterialCardView cardPassword;
+
+  @NonNull
   public final MaterialCardView cardViewImage;
 
   @NonNull
   public final CoordinatorLayout cordLayout;
+
+  @NonNull
+  public final TextInputEditText edPass;
+
+  @NonNull
+  public final TextInputEditText edUsername;
+
+  @NonNull
+  public final ImageView imageLock;
+
+  @NonNull
+  public final TextInputLayout layoutName;
+
+  @NonNull
+  public final TextInputLayout layoutPass;
+
+  @NonNull
+  public final LinearLayout layoutSubscriptionLevel;
 
   @NonNull
   public final ImageView profileImage;
@@ -36,18 +63,57 @@ public final class ActivityPersonalInformationBinding implements ViewBinding {
   public final LinearLayout relLayout;
 
   @NonNull
+  public final RelativeLayout relativeChangingColorOnClick;
+
+  @NonNull
+  public final TextView textUnlockSave;
+
+  @NonNull
+  public final TextView textView3;
+
+  @NonNull
+  public final TextView textViewActualLVL;
+
+  @NonNull
   public final MaterialToolbar toolbarInformation;
 
+  @NonNull
+  public final TextView tvUserMail;
+
+  @NonNull
+  public final TextView tvVariableActualSubscription;
+
   private ActivityPersonalInformationBinding(@NonNull RelativeLayout rootView,
+      @NonNull MaterialCardView cardName, @NonNull MaterialCardView cardPassword,
       @NonNull MaterialCardView cardViewImage, @NonNull CoordinatorLayout cordLayout,
+      @NonNull TextInputEditText edPass, @NonNull TextInputEditText edUsername,
+      @NonNull ImageView imageLock, @NonNull TextInputLayout layoutName,
+      @NonNull TextInputLayout layoutPass, @NonNull LinearLayout layoutSubscriptionLevel,
       @NonNull ImageView profileImage, @NonNull LinearLayout relLayout,
-      @NonNull MaterialToolbar toolbarInformation) {
+      @NonNull RelativeLayout relativeChangingColorOnClick, @NonNull TextView textUnlockSave,
+      @NonNull TextView textView3, @NonNull TextView textViewActualLVL,
+      @NonNull MaterialToolbar toolbarInformation, @NonNull TextView tvUserMail,
+      @NonNull TextView tvVariableActualSubscription) {
     this.rootView = rootView;
+    this.cardName = cardName;
+    this.cardPassword = cardPassword;
     this.cardViewImage = cardViewImage;
     this.cordLayout = cordLayout;
+    this.edPass = edPass;
+    this.edUsername = edUsername;
+    this.imageLock = imageLock;
+    this.layoutName = layoutName;
+    this.layoutPass = layoutPass;
+    this.layoutSubscriptionLevel = layoutSubscriptionLevel;
     this.profileImage = profileImage;
     this.relLayout = relLayout;
+    this.relativeChangingColorOnClick = relativeChangingColorOnClick;
+    this.textUnlockSave = textUnlockSave;
+    this.textView3 = textView3;
+    this.textViewActualLVL = textViewActualLVL;
     this.toolbarInformation = toolbarInformation;
+    this.tvUserMail = tvUserMail;
+    this.tvVariableActualSubscription = tvVariableActualSubscription;
   }
 
   @Override
@@ -77,6 +143,18 @@ public final class ActivityPersonalInformationBinding implements ViewBinding {
     // This is done to optimize the compiled bytecode for size and performance.
     int id;
     missingId: {
+      id = R.id.card_name;
+      MaterialCardView cardName = ViewBindings.findChildViewById(rootView, id);
+      if (cardName == null) {
+        break missingId;
+      }
+
+      id = R.id.card_password;
+      MaterialCardView cardPassword = ViewBindings.findChildViewById(rootView, id);
+      if (cardPassword == null) {
+        break missingId;
+      }
+
       id = R.id.cardViewImage;
       MaterialCardView cardViewImage = ViewBindings.findChildViewById(rootView, id);
       if (cardViewImage == null) {
@@ -86,6 +164,42 @@ public final class ActivityPersonalInformationBinding implements ViewBinding {
       id = R.id.cord_layout;
       CoordinatorLayout cordLayout = ViewBindings.findChildViewById(rootView, id);
       if (cordLayout == null) {
+        break missingId;
+      }
+
+      id = R.id.ed_pass;
+      TextInputEditText edPass = ViewBindings.findChildViewById(rootView, id);
+      if (edPass == null) {
+        break missingId;
+      }
+
+      id = R.id.ed_username;
+      TextInputEditText edUsername = ViewBindings.findChildViewById(rootView, id);
+      if (edUsername == null) {
+        break missingId;
+      }
+
+      id = R.id.imageLock;
+      ImageView imageLock = ViewBindings.findChildViewById(rootView, id);
+      if (imageLock == null) {
+        break missingId;
+      }
+
+      id = R.id.layout_name;
+      TextInputLayout layoutName = ViewBindings.findChildViewById(rootView, id);
+      if (layoutName == null) {
+        break missingId;
+      }
+
+      id = R.id.layout_pass;
+      TextInputLayout layoutPass = ViewBindings.findChildViewById(rootView, id);
+      if (layoutPass == null) {
+        break missingId;
+      }
+
+      id = R.id.layout_subscriptionLevel;
+      LinearLayout layoutSubscriptionLevel = ViewBindings.findChildViewById(rootView, id);
+      if (layoutSubscriptionLevel == null) {
         break missingId;
       }
 
@@ -101,14 +215,53 @@ public final class ActivityPersonalInformationBinding implements ViewBinding {
         break missingId;
       }
 
+      id = R.id.relativeChangingColorOnClick;
+      RelativeLayout relativeChangingColorOnClick = ViewBindings.findChildViewById(rootView, id);
+      if (relativeChangingColorOnClick == null) {
+        break missingId;
+      }
+
+      id = R.id.textUnlock_Save;
+      TextView textUnlockSave = ViewBindings.findChildViewById(rootView, id);
+      if (textUnlockSave == null) {
+        break missingId;
+      }
+
+      id = R.id.textView3;
+      TextView textView3 = ViewBindings.findChildViewById(rootView, id);
+      if (textView3 == null) {
+        break missingId;
+      }
+
+      id = R.id.textViewActualLVL;
+      TextView textViewActualLVL = ViewBindings.findChildViewById(rootView, id);
+      if (textViewActualLVL == null) {
+        break missingId;
+      }
+
       id = R.id.toolbarInformation;
       MaterialToolbar toolbarInformation = ViewBindings.findChildViewById(rootView, id);
       if (toolbarInformation == null) {
         break missingId;
       }
 
-      return new ActivityPersonalInformationBinding((RelativeLayout) rootView, cardViewImage,
-          cordLayout, profileImage, relLayout, toolbarInformation);
+      id = R.id.tv_userMail;
+      TextView tvUserMail = ViewBindings.findChildViewById(rootView, id);
+      if (tvUserMail == null) {
+        break missingId;
+      }
+
+      id = R.id.tv_variableActualSubscription;
+      TextView tvVariableActualSubscription = ViewBindings.findChildViewById(rootView, id);
+      if (tvVariableActualSubscription == null) {
+        break missingId;
+      }
+
+      return new ActivityPersonalInformationBinding((RelativeLayout) rootView, cardName,
+          cardPassword, cardViewImage, cordLayout, edPass, edUsername, imageLock, layoutName,
+          layoutPass, layoutSubscriptionLevel, profileImage, relLayout,
+          relativeChangingColorOnClick, textUnlockSave, textView3, textViewActualLVL,
+          toolbarInformation, tvUserMail, tvVariableActualSubscription);
     }
     String missingId = rootView.getResources().getResourceName(id);
     throw new NullPointerException("Missing required view with ID: ".concat(missingId));
--- WorkRoute/app/src/main/java/com/example/workroute/activitys/Personal_information.java
+++ WorkRoute/app/src/main/java/com/example/workroute/activitys/Personal_information.java
@@ -2,17 +2,35 @@ package com.example.workroute.activitys;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Person;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.example.workroute.R;
+import com.example.workroute.profile.Profile;
+import com.example.workroute.suscribtion.SubscribesActivity;
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Personal_information extends AppCompatActivity {
 
     private MaterialToolbar toolbar;
+    private RelativeLayout relativeChangeColors;
+    private ImageView img_lockChange;
+    private TextView tv_unlock_save,tv_user_mail;
+    private boolean sw = false;
+    private TextInputEditText ed_name,ed_password;
+    private LinearLayout ln_subscription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +43,14 @@ public class Personal_information extends AppCompatActivity {
         if (Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT) {
             getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         }
+
+
         main();
+        initData();
+    }
+
+    private void initData() {
+
     }
 
     private void main() {
@@ -35,6 +60,13 @@ public class Personal_information extends AppCompatActivity {
 
     private void controls() {
         toolbar = findViewById(R.id.toolbarInformation);
+        relativeChangeColors = findViewById(R.id.relativeChangingColorOnClick);
+        tv_unlock_save = findViewById(R.id.textUnlock_Save);
+        img_lockChange = findViewById(R.id.imageLock);
+        ed_name = findViewById(R.id.ed_username);
+        ed_password = findViewById(R.id.ed_pass);
+        ln_subscription = findViewById(R.id.layout_subscriptionLevel);
+        tv_user_mail = findViewById(R.id.tv_userMail);
     }
 
     private void initListeners() {
@@ -45,5 +77,39 @@ public class Personal_information extends AppCompatActivity {
                 finish();
             }
         });
+
+        relativeChangeColors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (sw==false) {
+                    sw=true;
+                    relativeChangeColors.setBackgroundColor(getColor(R.color.verde_flojo));
+                    img_lockChange.setImageResource(R.drawable.ic_bx_lock_open);
+                    tv_unlock_save.setText("SAVE CHANGES");
+                    ed_name.setEnabled(true);
+                    ed_password.setEnabled(true);
+                } else if(sw==true) {
+                    sw=false;
+                    relativeChangeColors.setBackgroundColor(Color.parseColor("#DC1C1C"));
+                    img_lockChange.setImageResource(R.drawable.ic_baseline_lock_24);
+                    tv_unlock_save.setText("UNLOCK FIELDS");
+                    ed_name.setEnabled(false);
+                    ed_password.setEnabled(false);
+                    //llamar a un metodo o algo de guardado de datos
+                }
+
+            }
+        });
+
+        ln_subscription.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    startActivity(new Intent(Personal_information.this, SubscribesActivity.class));
+            }
+        });
+
+
     }
+
 }
