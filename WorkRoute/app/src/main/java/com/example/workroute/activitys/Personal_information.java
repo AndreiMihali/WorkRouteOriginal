7a9b2f175c76efd6b286e818fa9cbd5e353ebe64
Andrei Mihali <amihali@d4dom.net>
2022-04-28 09:09:24 +0200

    added the change profile image option in personal information

--- WorkRoute/app/src/main/java/com/example/workroute/activitys/Personal_information.java
+++ WorkRoute/app/src/main/java/com/example/workroute/activitys/Personal_information.java
@@ -1,20 +1,33 @@
 package com.example.workroute.activitys;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.app.Person;
 import android.app.ProgressDialog;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.content.res.ColorStateList;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.webkit.MimeTypeMap;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -39,6 +52,14 @@ import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
+import java.util.HashMap;
 
 public class Personal_information extends AppCompatActivity {
 
@@ -53,6 +74,9 @@ public class Personal_information extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
     private SharedPreferences sp;
     private ProgressDialog progressDialog;
+    private ActivityResultLauncher<Intent> activityResultLauncher;
+    private final int CODE_GALLERY=100;
+    private Uri uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -133,6 +157,73 @@ public class Personal_information extends AppCompatActivity {
     private void main() {
         controls();
         initListeners();
+        initActivityResultLauncher();
+    }
+
+    private void initActivityResultLauncher() {
+        activityResultLauncher=registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if(result.getResultCode()==RESULT_OK){
+                            Intent photo=result.getData();
+                            uri=(Uri)photo.getData();
+                            updateImage();
+                            Bitmap bitmap= null;
+                            try {
+                                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),uri);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            imageProfile.setImageBitmap(bitmap);
+                        }
+                    }
+                }
+        );
+    }
+    
+
+    private void updateImage() {
+        new Handler().post(new Runnable() {
+            @Override
+            public void run() {
+                if(uri!=null){
+                    StorageReference reference= FirebaseStorage.getInstance().getReference().child("imagenesPerfil/"+System.currentTimeMillis()+"_"+getFileExtension(uri));
+                    reference.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Task<Uri> uriTask=taskSnapshot.getStorage().getDownloadUrl();
+                            while(!uriTask.isSuccessful());
+                            Uri downloadUri=uriTask.getResult();
+                            String downloadUrl=downloadUri.toString();
+                            HashMap<String,Object> hasMap=new HashMap<>();
+                            hasMap.put("fotoPerfil",downloadUrl);
+
+                            FirebaseFirestore.getInstance().collection("Usuarios").document(FirebaseAuth.getInstance().getUid()).update(hasMap).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+
+                                }
+                            });
+
+                            FirebaseDatabase.getInstance().getReference().child("Usuarios").child(FirebaseAuth.getInstance().getUid()).updateChildren(hasMap).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+
+                                }
+                            });
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    private String getFileExtension(Uri uri) {
+        ContentResolver contentResolver=getContentResolver();
+        MimeTypeMap mimeType=MimeTypeMap.getSingleton();
+        return mimeType.getExtensionFromMimeType(contentResolver.getType(uri));
     }
 
     private void controls() {
@@ -214,8 +305,44 @@ public class Personal_information extends AppCompatActivity {
                     startActivity(new Intent(Personal_information.this, SubscribesActivity.class));
             }
         });
+        
+        imageProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGallery();
+            }
+        });
 
 
+    }
+
+    private void openGallery() {
+        int  permissionCheck= ContextCompat.checkSelfPermission(this,android.Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        if(permissionCheck!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE},CODE_GALLERY);
+        }else{
+            callGallery();
+        }
+    }
+
+    private void callGallery() {
+        Intent gallery=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        activityResultLauncher.launch(gallery);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case CODE_GALLERY: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    callGallery();
+                }
+            }
+            break;
+        }
+        return;
     }
 
     private void setColorsFocus(TextInputEditText ed, TextInputLayout txt){
