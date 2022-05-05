2e63f86a712681b0557ab1d6db90a7748f635456
AndreiMihali <mihaliandrei50@gmail.com>
2022-05-05 11:37:45 +0200

    full

--- WorkRoute/app/src/main/java/com/example/workroute/activitys/AppInformation.java
+++ WorkRoute/app/src/main/java/com/example/workroute/activitys/AppInformation.java
@@ -1,9 +1,17 @@
 package com.example.workroute.activitys;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.example.workroute.R;
 import com.google.android.material.appbar.MaterialToolbar;
@@ -12,9 +20,27 @@ public class AppInformation extends AppCompatActivity {
     private MaterialToolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.Register);
+        Window window=getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS|WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        window.setStatusBarColor(Color.TRANSPARENT);
+        window.setNavigationBarColor(Color.TRANSPARENT);
+        setTheme(R.style.CustomTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_information);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.rel_layout_general),(v, windowInsets) -> {
+            Insets insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars());
+            Toolbar tool=findViewById(R.id.toolbarAppInfo);
+            ViewGroup.MarginLayoutParams mlp = (ViewGroup.MarginLayoutParams) tool.getLayoutParams();
+            mlp.leftMargin = insets.left;
+            mlp.bottomMargin = insets.bottom;
+            mlp.rightMargin = insets.right;
+            mlp.topMargin=insets.top;
+            tool.setLayoutParams(mlp);
+            return WindowInsetsCompat.CONSUMED;
+        });
         main();
     }
 
