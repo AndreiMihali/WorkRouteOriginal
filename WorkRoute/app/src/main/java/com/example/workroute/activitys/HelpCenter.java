2c4f6dae9daa0ce3c16aa22d69c1d53c4866299e
RubenDev7 <97059915+RubenDev7@users.noreply.github.com>
2022-05-05 22:13:00 +0200

    animaciones hechas, falta ver qué items se quedan y cuales no y poner los textos bien

--- WorkRoute/app/src/main/java/com/example/workroute/activitys/HelpCenter.java
+++ WorkRoute/app/src/main/java/com/example/workroute/activitys/HelpCenter.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.example.workroute.R;
 import com.example.workroute.network.callback.NetworkCallback;
@@ -31,6 +34,33 @@ public class HelpCenter extends AppCompatActivity {
      */
 
 
+    private ImageButton boton1;
+    private TextView txt1;
+    private ImageButton boton2;
+    private TextView txt2;
+    private ImageButton boton3;
+    private TextView txt3;
+    private ImageButton boton4;
+    private TextView txt4;
+    private ImageButton boton5;
+    private TextView txt5;
+    private ImageButton boton6;
+    private TextView txt6;
+    private ImageButton boton7;
+    private TextView txt7;
+    private ImageButton boton8;
+    private TextView txt8;
+    private ImageButton boton9;
+    private TextView txt9;
+    private ImageButton boton10;
+    private TextView txt10;
+    private ImageButton boton11;
+    private TextView txt11;
+    private ImageButton boton12;
+    private TextView txt12;
+    private ImageButton boton13;
+    private TextView txt13;
+
     private MaterialToolbar toolbar;
 
 
@@ -50,6 +80,58 @@ public class HelpCenter extends AppCompatActivity {
 
     private void controls() {
         toolbar = findViewById(R.id.toolbarUseWorkRoute);
+        boton1=findViewById(R.id.expandir_1);
+        txt1=findViewById(R.id.txt_1);
+        txt1.setVisibility(View.GONE);
+
+        boton2=findViewById(R.id.expandir_2);
+        txt2=findViewById(R.id.txt_2);
+        txt2.setVisibility(View.GONE);
+
+        boton3=findViewById(R.id.expandir_3);
+        txt3=findViewById(R.id.txt_3);
+        txt3.setVisibility(View.GONE);
+
+        boton4=findViewById(R.id.expandir_4);
+        txt4=findViewById(R.id.txt_4);
+        txt4.setVisibility(View.GONE);
+
+        boton5=findViewById(R.id.expandir_5);
+        txt5=findViewById(R.id.txt_5);
+        txt5.setVisibility(View.GONE);
+
+        boton6=findViewById(R.id.expandir_6);
+        txt6=findViewById(R.id.txt_6);
+        txt6.setVisibility(View.GONE);
+
+        boton7=findViewById(R.id.expandir_7);
+        txt7=findViewById(R.id.txt_7);
+        txt7.setVisibility(View.GONE);
+
+        boton8=findViewById(R.id.expandir_8);
+        txt8=findViewById(R.id.txt_8);
+        txt8.setVisibility(View.GONE);
+
+        boton9=findViewById(R.id.expandir_9);
+        txt9=findViewById(R.id.txt_9);
+        txt9.setVisibility(View.GONE);
+
+        boton10=findViewById(R.id.expandir_10);
+        txt10=findViewById(R.id.txt_10);
+        txt10.setVisibility(View.GONE);
+
+        boton11=findViewById(R.id.expandir_11);
+        txt11=findViewById(R.id.txt_11);
+        txt11.setVisibility(View.GONE);
+
+        boton12=findViewById(R.id.expandir_12);
+        txt12=findViewById(R.id.txt_12);
+        txt12.setVisibility(View.GONE);
+
+        boton13=findViewById(R.id.expandir_13);
+        txt13=findViewById(R.id.txt_13);
+        txt13.setVisibility(View.GONE);
+
     }
 
     private void initListeners() {
@@ -61,7 +143,188 @@ public class HelpCenter extends AppCompatActivity {
             }
         });
 
+        boton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(txt1.getVisibility()==View.GONE){
+                    txt1.setVisibility(View.VISIBLE);
+                    txt1.setAnimation(AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_up_in));
+                    boton1.setImageResource(R.drawable.bx_chevron_up);
+                }else if(txt1.getVisibility()==View.VISIBLE){
+                    txt1.setVisibility(View.GONE);
+                    boton1.setImageResource(R.drawable.bx_chevron_down);
+                }
+            }
+        });
+
+        boton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(txt2.getVisibility()==View.GONE){
+                    txt2.setVisibility(View.VISIBLE);
+                    txt2.setAnimation(AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_up_in));
+                    boton2.setImageResource(R.drawable.bx_chevron_up);
+                }else if(txt2.getVisibility()==View.VISIBLE){
+                    txt2.setVisibility(View.GONE);
+                    boton2.setImageResource(R.drawable.bx_chevron_down);
+                }
+            }
+        });
+
+        boton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(txt3.getVisibility()==View.GONE){
+                    txt3.setVisibility(View.VISIBLE);
+                    txt3.setAnimation(AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_up_in));
+                    boton3.setImageResource(R.drawable.bx_chevron_up);
+                }else if(txt3.getVisibility()==View.VISIBLE){
+                    txt3.setVisibility(View.GONE);
+                    boton3.setImageResource(R.drawable.bx_chevron_down);
+                }
+            }
+        });
+
+        boton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(txt4.getVisibility()==View.GONE){
+                    txt4.setVisibility(View.VISIBLE);
+                    txt4.setAnimation(AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_up_in));
+                    boton4.setImageResource(R.drawable.bx_chevron_up);
+                }else if(txt4.getVisibility()==View.VISIBLE){
+                    txt4.setVisibility(View.GONE);
+                    boton4.setImageResource(R.drawable.bx_chevron_down);
+                }
+            }
+        });
+
+        boton5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(txt5.getVisibility()==View.GONE){
+                    txt5.setVisibility(View.VISIBLE);
+                    txt5.setAnimation(AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_up_in));
+                    boton5.setImageResource(R.drawable.bx_chevron_up);
+                }else if(txt5.getVisibility()==View.VISIBLE){
+                    txt5.setVisibility(View.GONE);
+                    boton5.setImageResource(R.drawable.bx_chevron_down);
+                }
+            }
+        });
+
+
+        boton6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(txt6.getVisibility()==View.GONE){
+                    txt6.setVisibility(View.VISIBLE);
+                    txt6.setAnimation(AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_up_in));
+                    boton6.setImageResource(R.drawable.bx_chevron_up);
+                }else if(txt6.getVisibility()==View.VISIBLE){
+                    txt6.setVisibility(View.GONE);
+                    boton6.setImageResource(R.drawable.bx_chevron_down);
+                }
+            }
+        });
+
+        boton7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(txt7.getVisibility()==View.GONE){
+                    txt7.setVisibility(View.VISIBLE);
+                    txt7.setAnimation(AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_up_in));
+                    boton7.setImageResource(R.drawable.bx_chevron_up);
+                }else if(txt7.getVisibility()==View.VISIBLE){
+                    txt7.setVisibility(View.GONE);
+                    boton7.setImageResource(R.drawable.bx_chevron_down);
+                }
+            }
+        });
+
+        boton8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(txt8.getVisibility()==View.GONE){
+                    txt8.setVisibility(View.VISIBLE);
+                    txt8.setAnimation(AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_up_in));
+                    boton8.setImageResource(R.drawable.bx_chevron_up);
+                }else if(txt8.getVisibility()==View.VISIBLE){
+                    txt8.setVisibility(View.GONE);
+                    boton8.setImageResource(R.drawable.bx_chevron_down);
+                }
+            }
+        });
+
+        boton9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(txt9.getVisibility()==View.GONE){
+                    txt9.setVisibility(View.VISIBLE);
+                    txt9.setAnimation(AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_up_in));
+                    boton9.setImageResource(R.drawable.bx_chevron_up);
+                }else if(txt9.getVisibility()==View.VISIBLE){
+                    txt9.setVisibility(View.GONE);
+                    boton9.setImageResource(R.drawable.bx_chevron_down);
+                }
+            }
+        });
+
+        boton10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(txt10.getVisibility()==View.GONE){
+                    txt10.setVisibility(View.VISIBLE);
+                    txt10.setAnimation(AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_up_in));
+                    boton10.setImageResource(R.drawable.bx_chevron_up);
+                }else if(txt10.getVisibility()==View.VISIBLE){
+                    txt10.setVisibility(View.GONE);
+                    boton10.setImageResource(R.drawable.bx_chevron_down);
+                }
+            }
+        });
+
+        boton11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(txt11.getVisibility()==View.GONE){
+                    txt11.setVisibility(View.VISIBLE);
+                    txt11.setAnimation(AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_up_in));
+                    boton11.setImageResource(R.drawable.bx_chevron_up);
+                }else if(txt11.getVisibility()==View.VISIBLE){
+                    txt11.setVisibility(View.GONE);
+                    boton11.setImageResource(R.drawable.bx_chevron_down);
+                }
+            }
+        });
+
+        boton12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(txt12.getVisibility()==View.GONE){
+                    txt12.setVisibility(View.VISIBLE);
+                    txt12.setAnimation(AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_up_in));
+                    boton12.setImageResource(R.drawable.bx_chevron_up);
+                }else if(txt12.getVisibility()==View.VISIBLE){
+                    txt12.setVisibility(View.GONE);
+                    boton12.setImageResource(R.drawable.bx_chevron_down);
+                }
+            }
+        });
+
+        boton13.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(txt13.getVisibility()==View.GONE){
+                    txt13.setVisibility(View.VISIBLE);
+                    txt13.setAnimation(AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_up_in));
+                    boton13.setImageResource(R.drawable.bx_chevron_up);
+                }else if(txt13.getVisibility()==View.VISIBLE){
+                    txt13.setVisibility(View.GONE);
+                    boton13.setImageResource(R.drawable.bx_chevron_down);
+                }
+            }
+        });
+
     }
-
-
 }
