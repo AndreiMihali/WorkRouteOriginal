a5fd3c9073fee809d3209302d6c3d18ae274d475
AndreiMihali <mihaliandrei50@gmail.com>
2022-06-13 20:53:42 +0200

    changed

--- WorkRoute/app/build/tmp/kapt3/stubs/debug/com/example/workroute/kotlin/adapters/AdapterChatsList.java
+++ WorkRoute/app/build/tmp/kapt3/stubs/debug/com/example/workroute/kotlin/adapters/AdapterChatsList.java
@@ -2,16 +2,16 @@ package com.example.workroute.kotlin.adapters;
 
 import java.lang.System;
 
-@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000:\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0006\n\u0002\u0010\b\n\u0000\n\u0002\u0010\u0002\n\u0002\b\u0004\n\u0002\u0018\u0002\n\u0002\b\u0003\u0018\u00002\f\u0012\b\u0012\u00060\u0002R\u00020\u00000\u0001:\u0001\u0018B%\u0012\u0006\u0010\u0003\u001a\u00020\u0004\u0012\u0016\u0010\u0005\u001a\u0012\u0012\u0004\u0012\u00020\u00070\u0006j\b\u0012\u0004\u0012\u00020\u0007`\b\u00a2\u0006\u0002\u0010\tJ\b\u0010\u000e\u001a\u00020\u000fH\u0016J\u001c\u0010\u0010\u001a\u00020\u00112\n\u0010\u0012\u001a\u00060\u0002R\u00020\u00002\u0006\u0010\u0013\u001a\u00020\u000fH\u0016J\u001c\u0010\u0014\u001a\u00060\u0002R\u00020\u00002\u0006\u0010\u0015\u001a\u00020\u00162\u0006\u0010\u0017\u001a\u00020\u000fH\u0016R\u0011\u0010\u0003\u001a\u00020\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\n\u0010\u000bR!\u0010\u0005\u001a\u0012\u0012\u0004\u0012\u00020\u00070\u0006j\b\u0012\u0004\u0012\u00020\u0007`\b\u00a2\u0006\b\n\u0000\u001a\u0004\b\f\u0010\r\u00a8\u0006\u0019"}, d2 = {"Lcom/example/workroute/kotlin/adapters/AdapterChatsList;", "Landroidx/recyclerview/widget/RecyclerView$Adapter;", "Lcom/example/workroute/kotlin/adapters/AdapterChatsList$ViewHolder;", "context", "Landroid/content/Context;", "data", "Ljava/util/ArrayList;", "Lcom/example/workroute/kotlin/model/UserChatModel;", "Lkotlin/collections/ArrayList;", "(Landroid/content/Context;Ljava/util/ArrayList;)V", "getContext", "()Landroid/content/Context;", "getData", "()Ljava/util/ArrayList;", "getItemCount", "", "onBindViewHolder", "", "holder", "position", "onCreateViewHolder", "parent", "Landroid/view/ViewGroup;", "viewType", "ViewHolder", "app_debug"})
+@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u00006\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010!\n\u0002\u0018\u0002\n\u0002\b\u0006\n\u0002\u0010\b\n\u0000\n\u0002\u0010\u0002\n\u0002\b\u0004\n\u0002\u0018\u0002\n\u0002\b\u0003\u0018\u00002\f\u0012\b\u0012\u00060\u0002R\u00020\u00000\u0001:\u0001\u0017B\u001b\u0012\u0006\u0010\u0003\u001a\u00020\u0004\u0012\f\u0010\u0005\u001a\b\u0012\u0004\u0012\u00020\u00070\u0006\u00a2\u0006\u0002\u0010\bJ\b\u0010\r\u001a\u00020\u000eH\u0016J\u001c\u0010\u000f\u001a\u00020\u00102\n\u0010\u0011\u001a\u00060\u0002R\u00020\u00002\u0006\u0010\u0012\u001a\u00020\u000eH\u0016J\u001c\u0010\u0013\u001a\u00060\u0002R\u00020\u00002\u0006\u0010\u0014\u001a\u00020\u00152\u0006\u0010\u0016\u001a\u00020\u000eH\u0016R\u0011\u0010\u0003\u001a\u00020\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\t\u0010\nR\u0017\u0010\u0005\u001a\b\u0012\u0004\u0012\u00020\u00070\u0006\u00a2\u0006\b\n\u0000\u001a\u0004\b\u000b\u0010\f\u00a8\u0006\u0018"}, d2 = {"Lcom/example/workroute/kotlin/adapters/AdapterChatsList;", "Landroidx/recyclerview/widget/RecyclerView$Adapter;", "Lcom/example/workroute/kotlin/adapters/AdapterChatsList$ViewHolder;", "context", "Landroid/content/Context;", "data", "", "Lcom/example/workroute/kotlin/model/UserChatModel;", "(Landroid/content/Context;Ljava/util/List;)V", "getContext", "()Landroid/content/Context;", "getData", "()Ljava/util/List;", "getItemCount", "", "onBindViewHolder", "", "holder", "position", "onCreateViewHolder", "parent", "Landroid/view/ViewGroup;", "viewType", "ViewHolder", "app_debug"})
 public final class AdapterChatsList extends androidx.recyclerview.widget.RecyclerView.Adapter<com.example.workroute.kotlin.adapters.AdapterChatsList.ViewHolder> {
     @org.jetbrains.annotations.NotNull()
     private final android.content.Context context = null;
     @org.jetbrains.annotations.NotNull()
-    private final java.util.ArrayList<com.example.workroute.kotlin.model.UserChatModel> data = null;
+    private final java.util.List<com.example.workroute.kotlin.model.UserChatModel> data = null;
     
     public AdapterChatsList(@org.jetbrains.annotations.NotNull()
     android.content.Context context, @org.jetbrains.annotations.NotNull()
-    java.util.ArrayList<com.example.workroute.kotlin.model.UserChatModel> data) {
+    java.util.List<com.example.workroute.kotlin.model.UserChatModel> data) {
         super();
     }
     
@@ -21,7 +21,7 @@ public final class AdapterChatsList extends androidx.recyclerview.widget.Recycle
     }
     
     @org.jetbrains.annotations.NotNull()
-    public final java.util.ArrayList<com.example.workroute.kotlin.model.UserChatModel> getData() {
+    public final java.util.List<com.example.workroute.kotlin.model.UserChatModel> getData() {
         return null;
     }
     
